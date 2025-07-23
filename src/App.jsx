7dddb830fc952136c48d95bd12eeb40d8e532e28
Shravani-87 <shravanipatil921@gmail.com>
2025-07-23 18:16:19 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () =>{
         <DeveloperInfoPopup
           show={showPopup}
           onClose={handleClosePopup}
-          studentName="Saniya & Shravani"
+          studentName="Shravani"
           studentPhotoUrl="/images/shravani.jpg" // Path to their photo
           uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
         />
